daedb96ea55cd437783cb2646fc38be00f61789e
aghaznaw <aghaznaw@W10-155.student.yo.oulu.fi>
2018-09-12 18:13:20 +0300

    Test

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	
 		TennisGame play = new TennisGame();
 		play.getScore();
 	}
--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,34 +1,65 @@
+import java.util.Scanner;
 
 public class TennisGame {
 	
 	private int p1;
 	private int p2;
-	private int scoreFlag;
+	private int scoreFlagP1;
+	private int scoreFlagP2;
 	
 	public TennisGame() {
 		// TO BE IMPLEMENTED
 		System.out.println("I am constructor");
 		
+		
 	}
 
-	public String getScore() {
+	public void  getScore() {
 		System.out.println("I am method for displaying score");
-		// Here is the format of the scores: "player1Score - player2Score"
-		// "0 - 0"
-		// "15 - 15"
-		// "30 - 30"
-		// "deuce"
-		// "15 - 0", "0 - 15"
-		// "30 - 0", "0 - 30"
-		// "40 - 0", "0 - 40"
-		// "30 - 15", "15 - 30"
-		// "40 - 15", "15 - 40"
-		// "advantage player1"
-		// "advantage player2"
-		// "game player1"
-		// "game player2"
-
-		// TO BE IMPLEMENTED
-		return "";
+			
+		while(this.checkWiner() != "win") {
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the score for P1");
+		    int score = input.nextInt();
+			
+			if(score != 0) {
+				p1 = p1 + score;
+				scoreFlagP1 += 1;
+				
+			}else{
+				p2 = p2 + 15;
+				scoreFlagP2 += 1;
+			}
+		   
+		
+			System.out.println("p1 - p2");
+			System.out.println(p1 + " - " + p2);
+			
+			// Here is the format of the scores: "player1Score - player2Score"
+			// "0 - 0"
+			// "15 - 15"
+			// "30 - 30"
+			// "deuce"
+			// "15 - 0", "0 - 15"
+			// "30 - 0", "0 - 30"
+			// "40 - 0", "0 - 40"
+			// "30 - 15", "15 - 30"
+			// "40 - 15", "15 - 40"
+			// "advantage player1"
+			// "advantage player2"
+			// "game player1"
+			// "game player2"
+	
+			// TO BE IMPLEMENTED
+		}
+	}
+	
+	private String checkWiner() {
+		
+		String winer = "";
+		if(scoreFlagP1 < scoreFlagP2 + 2 ) {
+			
+		}
+		return "noWin";
 	}
 }
